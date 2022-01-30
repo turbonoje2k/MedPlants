1ac812381467ec75fbb85f14e4cc081c44c16e7e
turboNoje2K <9hnoje@gmail.com>
2022-01-30 22:05:18 +0100

    Initial Commit

---
+++ MedPlants/MedPlantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedPlantsApp.swift
+//  MedPlants
+//
+//  Created by noje on 30/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct MedPlantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
